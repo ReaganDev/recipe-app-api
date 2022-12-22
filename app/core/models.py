eb7e8078d1e7a0bf84e2219217986e8d662f96d5
ReaganDev <Charixma95@gmail.com>
2022-12-22 12:28:54 +0100

    create superuser

--- app/core/models.py
+++ app/core/models.py
@@ -20,6 +20,18 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        # if extra_fields.get("is_staff") is not True:
+        #     raise ValueError("Superuser must have is_staff=True.")
+        # if extra_fields.get("is_superuser") is not True:
+        #     raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """user in the system"""
