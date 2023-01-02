27897c46421979fbae2e47a45e7d55a5ff540d19
ReaganDev <Charixma95@gmail.com>
2023-01-02 18:00:36 +0100

    implement create user endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from user.serializer import UserSerializer
 
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
