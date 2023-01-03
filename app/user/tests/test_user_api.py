6825e9b820241f5f3f246ebbd2f7cc90c037fe05
ReaganDev <Charixma95@gmail.com>
2023-01-03 14:17:42 +0100

    changes

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -23,11 +23,11 @@ class PublicUserApiTests(TestCase):
             'password': 'test123',
             'name': 'Reagan'
         }
-        res = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, **payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
-        self.assertEqual(user.check_password(payload['password']))
+        self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists(self):
@@ -37,7 +37,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Reagan'
         }
         create_user(**payload)
-        res = self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, **payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
