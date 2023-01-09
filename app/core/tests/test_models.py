5a675ae49b9ff545f6aa52c6c03cb9925f08a555
ReaganDev <Charixma95@gmail.com>
2023-01-09 13:28:07 +0100

    Add tag model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,10 @@ from django.contrib.auth import get_user_model
 from core.models import User
 
 
+def create_user(email='user@example.com', password='Reagan'):
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTests(TestCase):
     """TEST MODELS"""
 
@@ -58,3 +62,9 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag 1')
+
+        self.assertEqual(str(tag), tag.name)
